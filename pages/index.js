cf1c858ba00f07153549d2571a7441d2cd9b6aca
kharperdils <kharper@divcore.com>
2026-04-26 22:01:15 -0700

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#102a4c", color: "white", padding: "80px" }}>
+      <h1>DivCore Website Prototype</h1>
+      <p>Initial homepage is working.</p>
+    </main>
+  );
+}
